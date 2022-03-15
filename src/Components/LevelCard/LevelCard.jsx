0103d31109a7aae29cdb8bd247d858e7f2510059
Matt Wilson <mattwwilson34@gmail.com>
2022-03-14 20:59:32 -0400

    Transfered inline styles in jsx to css file

--- src/Components/LevelCard/LevelCard.jsx
+++ src/Components/LevelCard/LevelCard.jsx
@@ -10,9 +10,6 @@ const LevelCard = (props) => {
       className='LevelCard'
       style={{
         backgroundImage: `url(${img})`,
-        backgroundSize: '100%',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
       }}>
       <Link to='#'>
         <span className='LevelCard-Title'>{title}</span>
