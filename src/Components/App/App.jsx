9b35d77470e24d11928fe4ca5e1f73a469cddbbf
Matt Wilson <mattwwilson34@gmail.com>
2022-03-14 20:51:34 -0400

    Imported Gamecube img and passed to LevelCard

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -2,12 +2,13 @@ import './App.css';
 import React from 'react';
 import Nav from '../Nav';
 import LevelCard from '../LevelCard';
+import Gamecube from '../../Images/Gamecube.jpg';
 
 const App = () => {
   return (
     <div>
       <Nav />
-      <LevelCard title='Gamecube' />
+      <LevelCard title='Gamecube' img={Gamecube} />
     </div>
   );
 };
