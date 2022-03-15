cca59ba5483d21412dfa8089bacf5804cedaf4d7
Matt Wilson <mattwwilson34@gmail.com>
2022-03-14 21:26:34 -0400

    Cleaned up imports

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -2,8 +2,6 @@ import './App.css';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Nav from '../Nav';
-import LevelCard from '../LevelCard';
-import Gamecube from '../../Images/Gamecube.jpg';
 
 const App = () => {
   return (
--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './Components/App';
+import Game from './Components/Game';
+import Levels from './Routes/Levels';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path='/' element={<App />}>
+          <Route path='/' element={<Levels />} />
+          <Route path='game' element={<Game />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
