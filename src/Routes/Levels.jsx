a0d3a5eee784f5b949f2a9a1151af36c7f0151c9
Matt Wilson <mattwwilson34@gmail.com>
2022-03-14 21:24:19 -0400

    Added Levels route

---
+++ src/Routes/Levels.jsx
@@ -0,0 +1,8 @@
+import LevelCard from '../Components/LevelCard';
+import Gamecube from '../Images/Gamecube.jpg';
+
+const Levels = () => {
+  return <LevelCard img={Gamecube} />;
+};
+
+export default Levels;
