279042c7cbfc611ffab34b8534961e3730e88c03
Matt Wilson <mattwwilson34@gmail.com>
2022-03-14 20:50:52 -0400

    Destructured background img and added inline styles

--- src/Components/LevelCard/LevelCard.jsx
+++ src/Components/LevelCard/LevelCard.jsx
@@ -3,10 +3,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const LevelCard = (props) => {
-  const { title } = props;
+  const { title, img } = props;
 
   return (
-    <div className='LevelCard'>
+    <div
+      className='LevelCard'
+      style={{
+        backgroundImage: `url(${img})`,
+        backgroundSize: '100%',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+      }}>
       <Link to='#'>
         <span className='LevelCard-Title'>{title}</span>
       </Link>
