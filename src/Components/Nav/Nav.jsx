e18ca6e2b60921c6433bdd79f33c9f13ffe68397
Matt Wilson <mattwwilson34@gmail.com>
2022-03-14 20:13:16 -0400

    Added Nav component

---
+++ src/Components/Nav/Nav.jsx
@@ -0,0 +1,8 @@
+import './Nav.css'
+import React from 'react';
+
+const Nav = () => {
+  return <div>Nav COMPONENT</div>;
+  }; 
+
+  export default Nav;
---
+++ src/Components/Nav/__test__/Nav.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Nav from '../Nav';
+
+describe('Nav component', () => {
+  it('Does something...', () => {
+
+  });
+});
