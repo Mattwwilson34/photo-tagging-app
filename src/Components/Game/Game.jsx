bdd947d450465eaad3138f927baecf5108c21245
Matt Wilson <mattwwilson34@gmail.com>
2022-03-14 21:40:20 -0400

    Added Gamecube image

--- src/Components/Game/Game.jsx
+++ src/Components/Game/Game.jsx
@@ -1,8 +1,13 @@
-import './Game.css'
+import './Game.css';
 import React from 'react';
+import Gamecube from '../../Images/Gamecube.jpg';
 
 const Game = () => {
-  return <div>Game COMPONENT</div>;
-  }; 
+  return (
+    <div className='Game-Container'>
+      <img className='Game-Img' src={Gamecube} alt='Gamecube'></img>
+    </div>
+  );
+};
 
-  export default Game;
+export default Game;
