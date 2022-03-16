e4ca65368fff972206f2eb7f6da7f40fcdf6df1b
Matt Wilson <mattwwilson34@gmail.com>
2022-03-16 19:48:36 -0400

    Added TargetArea Component

---
+++ src/Components/TargetArea/TargetArea.jsx
@@ -0,0 +1,8 @@
+import './TargetArea.css'
+import React from 'react';
+
+const TargetArea = () => {
+  return <div>TargetArea COMPONENT</div>;
+  }; 
+
+  export default TargetArea;
---
+++ src/Components/TargetArea/__test__/TargetArea.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import TargetArea from '../TargetArea';
+
+describe('TargetArea component', () => {
+  it('Does something...', () => {
+
+  });
+});
