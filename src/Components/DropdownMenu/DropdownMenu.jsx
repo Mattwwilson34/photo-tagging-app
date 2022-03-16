d3e17849f70d2e772ed643006bfc7853dba8b437
Matt Wilson <mattwwilson34@gmail.com>
2022-03-15 20:16:21 -0400

    Added dropDown component

---
+++ src/Components/DropdownMenu/DropdownMenu.jsx
@@ -0,0 +1,16 @@
+import './DropdownMenu.css';
+import React from 'react';
+
+const DropdownMenu = () => {
+  return (
+    <div>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </div>
+  );
+};
+
+export default DropdownMenu;
