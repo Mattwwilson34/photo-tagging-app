16c2e93f39286ff62a604eae064cc3c16e08d34d
Matt Wilson <mattwwilson34@gmail.com>
2022-03-18 19:31:37 -0400

    Added GameStartOverlay component

---
+++ src/Components/GameStartOverlay/GameStartOverlay.jsx
@@ -0,0 +1,8 @@
+import './GameStartOverlay.css'
+import React from 'react';
+
+const GameStartOverlay = () => {
+  return <div>GameStartOverlay COMPONENT</div>;
+  }; 
+
+  export default GameStartOverlay;
---
+++ src/Components/GameStartOverlay/__test__/GameStartOverlay.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import GameStartOverlay from '../GameStartOverlay';
+
+describe('GameStartOverlay component', () => {
+  it('Does something...', () => {
+
+  });
+});
