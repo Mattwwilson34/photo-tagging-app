c5d13fedb4d864e0ed8d194b2298f1aa120e5da3
Matt Wilson <mattwwilson34@gmail.com>
2022-03-18 08:16:11 -0400

    Added MessageOverlay component

---
+++ src/Components/MessageOverlay/MessageOverlay.jsx
@@ -0,0 +1,8 @@
+import './MessageOverlay.css'
+import React from 'react';
+
+const MessageOverlay = () => {
+  return <div>MessageOverlay COMPONENT</div>;
+  }; 
+
+  export default MessageOverlay;
---
+++ src/Components/MessageOverlay/__test__/MessageOverlay.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import MessageOverlay from '../MessageOverlay';
+
+describe('MessageOverlay component', () => {
+  it('Does something...', () => {
+
+  });
+});
