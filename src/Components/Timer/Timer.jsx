f06ca0a77db4c08d781a102ec95ab19d785c2987
Matt Wilson <mattwwilson34@gmail.com>
2022-03-18 20:04:43 -0400

    Added on,off,reset button and state

--- src/Components/Timer/Timer.jsx
+++ src/Components/Timer/Timer.jsx
@@ -3,15 +3,30 @@ import React, { useState, useEffect } from 'react';
 
 const Timer = () => {
   const [time, setTime] = useState(0);
+  const [timeOn, setTimeOn] = useState(false);
 
   useEffect(() => {
-    setTimeout(() => {
-      setTime(time + 1);
-    }, 1000);
-    return clearTimeout(setTimeout);
-  }, [time]);
+    let interval = null;
 
-  return <div className='Timer'>{time}</div>;
+    if (timeOn) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 10);
+      }, 10);
+    } else {
+      clearInterval(interval);
+    }
+  }, [timeOn]);
+
+  return (
+    <div className='Timer'>
+      {time}
+      <div>
+        <button onClick={setTimeOn(true)}>On</button>
+        <button onClick={setTimeOn(false)}>Off</button>
+        <button onClick={setTime(0)}>Reset</button>
+      </div>
+    </div>
+  );
 };
 
 export default Timer;
