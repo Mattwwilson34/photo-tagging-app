1ba98bc567a147c474a741fc71ff37a76158b015
Matt Wilson <mattwwilson34@gmail.com>
2022-03-20 14:05:16 -0400

    Added copy, and elements class names

--- src/Components/GameOver/GameOver.jsx
+++ src/Components/GameOver/GameOver.jsx
@@ -1,8 +1,20 @@
-import './GameOver.css'
+import './GameOver.css';
 import React from 'react';
 
 const GameOver = () => {
-  return <div>GameOver COMPONENT</div>;
-  }; 
+  return (
+    <div className='GameOver'>
+      <div>
+        <p>You found all the Characters!</p>
+        <p>Enter your name below to link it to your score</p>
+        <input type='text' classname='GameOver-UserInput'></input>
+        <span className='GameOver-Disclaimer'>
+          If you do not specify a name your score will be listed under Anonymous
+        </span>
+        <button>Submit</button>
+      </div>
+    </div>
+  );
+};
 
-  export default GameOver;
+export default GameOver;
