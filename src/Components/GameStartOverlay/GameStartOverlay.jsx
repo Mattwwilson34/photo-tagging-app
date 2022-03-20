fbde62ccbc31bc46d7d83814edfc570f134dd129
Matt Wilson <mattwwilson34@gmail.com>
2022-03-20 12:16:51 -0400

    Added setUser func and firebase imports

--- src/Components/GameStartOverlay/GameStartOverlay.jsx
+++ src/Components/GameStartOverlay/GameStartOverlay.jsx
@@ -1,16 +1,36 @@
 import './GameStartOverlay.css';
 import React from 'react';
-import { setUser } from '../../Utils/setUser';
+import firebaseApp from '../../Firebase/Firebase';
+import {
+  collection,
+  addDoc,
+  serverTimestamp,
+  getFirestore,
+} from 'firebase/firestore';
 
 const GameStartOverlay = (props) => {
-  const { setGameStarted, setTimerOn, setCurrentUser } = props;
+  const { setGameStarted, setTimerOn, setCurrentUserID } = props;
+
+  // Setting DB
+  const db = getFirestore(firebaseApp);
+
+  // Initialize user
+  const setUser = async () => {
+    const userRef = await addDoc(collection(db, 'Users'), {
+      name: 'Anonymous',
+      timeStart: serverTimestamp(),
+    });
+    console.log('Anonymous User + start time added w/ ID: ', userRef.id);
+    setCurrentUserID(userRef.id);
+  };
+
   return (
     <div className='GameStartOverlay'>
       <div>
         <span>Click the button when you are ready.</span>
         <button
           onClick={() => {
-            setCurrentUser(setUser());
+            setUser();
             setGameStarted(true);
             setTimerOn(true);
           }}>
