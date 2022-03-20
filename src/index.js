55acbaf19bbe21b2483d664ab8faf3e9235cb8cc
Matt Wilson <mattwwilson34@gmail.com>
2022-03-20 15:31:03 -0400

    Added leaderboard route

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import './index.css';
 import App from './Components/App';
 import Game from './Components/Game';
 import Levels from './Routes/Levels';
+import Leaderboard from './Components/Leaderboard';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,6 +14,7 @@ ReactDOM.render(
         <Route path='/' element={<App />}>
           <Route path='/' element={<Levels />} />
           <Route path='game' element={<Game />} />
+          <Route path='leaderboard' element={<Leaderboard />} />
         </Route>
       </Routes>
     </BrowserRouter>
