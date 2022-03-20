53d7f073b356698662c3d74dbeb8acf93860eebc
Matt Wilson <mattwwilson34@gmail.com>
2022-03-20 11:09:36 -0400

    Added setUser func and setCurrentUser state funcs

--- src/Components/GameStartOverlay/GameStartOverlay.jsx
+++ src/Components/GameStartOverlay/GameStartOverlay.jsx
@@ -1,14 +1,16 @@
 import './GameStartOverlay.css';
 import React from 'react';
+import { setUser } from '../../Utils/setUser';
 
 const GameStartOverlay = (props) => {
-  const { setGameStarted, setTimerOn } = props;
+  const { setGameStarted, setTimerOn, setCurrentUser } = props;
   return (
     <div className='GameStartOverlay'>
       <div>
         <span>Click the button when you are ready.</span>
         <button
           onClick={() => {
+            setCurrentUser(setUser());
             setGameStarted(true);
             setTimerOn(true);
           }}>
