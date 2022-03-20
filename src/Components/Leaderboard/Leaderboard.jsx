82848fc11e4cf1013e5d887a9574a560550d0bfc
Matt Wilson <mattwwilson34@gmail.com>
2022-03-20 16:43:05 -0400

    Added firebase DB data pull and table markup

--- src/Components/Leaderboard/Leaderboard.jsx
+++ src/Components/Leaderboard/Leaderboard.jsx
@@ -1,8 +1,55 @@
-import './Leaderboard.css'
-import React from 'react';
+import './Leaderboard.css';
+import React, { useState, useEffect } from 'react';
+import firebaseApp from '../../Firebase/Firebase';
+import {
+  getFirestore,
+  collection,
+  query,
+  getDocs,
+  orderBy,
+} from 'firebase/firestore';
 
 const Leaderboard = () => {
-  return <div>Leaderboard COMPONENT</div>;
-  }; 
+  const [users, setUsers] = useState(null);
 
-  export default Leaderboard;
+  useEffect(() => {
+    (async () => {
+      const usersArray = [];
+      const db = getFirestore(firebaseApp);
+      const usersRef = query(collection(db, 'Users'));
+      const q = query(usersRef, orderBy('totalTime'));
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        usersArray.push(doc.data());
+      });
+      setUsers(usersArray);
+    })();
+  }, []);
+
+  return (
+    <div className='Leaderboard'>
+      <table cellSpacing={0}>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Score(s)</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users
+            ? users.map((user) => {
+                return (
+                  <tr>
+                    <td>{user.name}</td>
+                    <td>{user.totalTime}</td>
+                  </tr>
+                );
+              })
+            : 'Loading'}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Leaderboard;
