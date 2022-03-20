d50209cf04c9b6e746b464eabe41ca32523232f9
Matt Wilson <mattwwilson34@gmail.com>
2022-03-20 15:28:17 -0400

    Changed Links to NavLinks

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,13 +1,22 @@
 import './Nav.css';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Nav = () => {
+  let activeStyle = {
+    textDecoration: 'underline',
+    color: 'red',
+  };
+
   return (
     <nav className='Nav'>
-      <Link to='#'>Info</Link>
+      <NavLink to='#'>Info</NavLink>
       <span className='Nav-Title'>Photo Tagging App</span>
-      <Link to='#'>Leaderboard</Link>
+      <NavLink
+        to='leaderboard'
+        style={({ isActive }) => (isActive ? activeStyle : undefined)}>
+        Leaderboard
+      </NavLink>
     </nav>
   );
 };
