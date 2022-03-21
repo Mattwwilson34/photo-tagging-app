9ce0e02ff74027b9ad70a98eb965b320a05b58a3
Matt Wilson <mattwwilson34@gmail.com>
2022-03-20 20:58:49 -0400

    Added How to play copy

--- src/Components/LevelCard/LevelCard.jsx
+++ src/Components/LevelCard/LevelCard.jsx
@@ -14,6 +14,33 @@ const LevelCard = (props) => {
         }}>
         <Link to='/game'></Link>
       </div>
+      <div className='LevelCard-GameplayInfo'>
+        <h2>How to play</h2>
+        <ol>
+          <li>Select a level card from above.</li>
+          <li>This will bring you to the game start page.</li>
+          <li>Click the "Start Game" button to begin the game.</li>
+          <li>
+            As quickly as you can, locate the levels special characters in the
+            image.
+          </li>
+          <ul>
+            <li>Clicking on the image places a circle around your cursor.</li>
+            <li>
+              Clicking on the image also displays a dropdown menu of characters
+              you can choose from.
+            </li>
+          </ul>
+          <li>
+            Once all the characters are found the game is over and the timer
+            stops.
+          </li>
+          <li>
+            You can then entire your name if you would like to be displayed on
+            the leaderboard.
+          </li>
+        </ol>
+      </div>
     </div>
   );
 };
