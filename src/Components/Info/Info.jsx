05f8e9eee508dc2c61695c833bddd500ae907373
Matt Wilson <mattwwilson34@gmail.com>
2022-03-20 20:14:21 -0400

    Fixed typos

--- src/Components/Info/Info.jsx
+++ src/Components/Info/Info.jsx
@@ -5,56 +5,52 @@ import { SocialIcon } from 'react-social-icons';
 const Info = () => {
   return (
     <div className='Info'>
-      <h1>Photo tagging app</h1>
-      <span>
-        Created by: Matt Wilson
-        <SocialIcon url='https://github.com/Mattwwilson34' />
-      </span>
+      <h1>
+        Photo tagging app <SocialIcon url='https://github.com/Mattwwilson34' />
+      </h1>
+      <span className='Info-CreatedBy'>Created by: Matt Wilson</span>
+      <h2>Description:</h2>
       <p>
         This app was built as part of the Odin projecs JavaScript course. This
-        project was the most complex project to date and required use of all
-        content learned in the course up to this poit inlcluding:
-        <ul>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>Standard JavaScript</li>
-          <li>ReactJS</li>
-          <li>React Router</li>
-          <li>Firebase Firestore</li>
-          <li>Jest</li>
-          <li>React testing-library</li>
-        </ul>
+        project was the most complex project to date and required utilizing all
+        the skills we had learned throughout the course inlcluding:
       </p>
+      <ul>
+        <li>HTML, CSS, JS</li>
+        <li>ReactJS</li>
+        <li>React Router</li>
+        <li>Firebase Firestore</li>
+        <li>Jest</li>
+        <li>React testing-library</li>
+      </ul>
 
-      <p>
-        Some of the unique challenges to this project included:
+      <h2>Some of the unique challenges to this project included:</h2>
+      <ul>
+        <li>Managing large images without distortion</li>
+        <li>Image coordinate calculations</li>
         <ul>
-          <li>Managing large images without distortion</li>
-          <li>Image coordinate calculations</li>
-          <ul>
-            <li>Calculate locations of characters within image</li>
-            <li>
-              Make sure coordinates are relative an not effected by screen
-              size/resolution
-            </li>
-            <li>Get real time coordinates when use clicks on image</li>
-          </ul>
+          <li>Calculate locations of characters within image</li>
           <li>
-            Store static character coordinates in firebase firestore database
+            Make sure coordinates are relative and not effected by screen
+            size/resolution
           </li>
-          <li>
-            Compare users clicked coordinates to correct database coordinates
-          </li>
-          <li>Implement a timer that begins when the game starts</li>
-          <ul>
-            <li>Store timer data in database to prevent user tampering</li>
-          </ul>
-          <li>
-            Handle both anonymous users as well as allow users to input a
-            username to store their score on the leaderboad
-          </li>
+          <li>Get real time coordinates when users click on the image</li>
         </ul>
-      </p>
+        <li>
+          Store static character coordinates in firebase firestore database
+        </li>
+        <li>
+          Compare users clicked coordinates to correct database coordinates
+        </li>
+        <li>Implement a timer that begins when the game starts</li>
+        <ul>
+          <li>Store timer data in database to prevent user tampering</li>
+        </ul>
+        <li>
+          Handle both anonymous users as well as allow users to input a username
+          to store their score on the leaderboad
+        </li>
+      </ul>
     </div>
   );
 };
