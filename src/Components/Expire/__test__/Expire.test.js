3bf6d45e5d21d04402f7bd7a590c0c48833fa98e
Matt Wilson <mattwwilson34@gmail.com>
2022-03-22 19:58:32 -0400

    Removed uneeded components

--- src/Components/Expire/__test__/Expire.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Expire from '../Expire';
-
-describe('Expire component', () => {
-  it('Does something...', () => {
-
-  });
-});
--- src/Components/Timer/__test__/Timer.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Timer from '../Timer';
-
-describe('Timer component', () => {
-  it('Does something...', () => {
-
-  });
-});
