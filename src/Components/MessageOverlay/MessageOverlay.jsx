92dbc23e91cbb6087b64bf57bc5e99530f0fe106
Matt Wilson <mattwwilson34@gmail.com>
2022-03-22 19:50:56 -0400

    Fixed conditional logic

--- src/Components/MessageOverlay/MessageOverlay.jsx
+++ src/Components/MessageOverlay/MessageOverlay.jsx
@@ -1,19 +1,38 @@
 import './MessageOverlay.css';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const MessageOverlay = (props) => {
-  const { message, mouseX, mouseY } = props;
+  const { message, mouseX, mouseY, setDropdownClicked, setCorrect } = props;
+  const [visible, setVisible] = useState(true);
 
-  return (
-    <div
-      className='MessageOverlay'
-      style={{
-        left: `${mouseX - 75}px`,
-        top: `${mouseY - 125}px`,
-      }}>
-      {message}
-    </div>
-  );
+  const correct = {
+    left: `${mouseX - 75}px`,
+    top: `${mouseY - 125}px`,
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setVisible(false);
+      setDropdownClicked(false);
+      setCorrect(false);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, [visible, setDropdownClicked, setCorrect]);
+
+  if (visible) {
+    return (
+      <div
+        className={`MessageOverlay ${
+          message === 'Correct' ? 'correct' : 'incorrect'
+        }`}
+        style={correct}>
+        {message}
+      </div>
+    );
+  } else {
+    return null;
+  }
 };
 
 export default MessageOverlay;
