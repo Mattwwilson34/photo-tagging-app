d3e4a1fac982901c73be75142c55c88b558a8eff
Matt Wilson <mattwwilson34@gmail.com>
2022-03-22 20:13:09 -0400

    Initial commit

---
+++ src/Components/CharacterKey/CharacterKey.jsx
@@ -0,0 +1,8 @@
+import './CharacterKey.css'
+import React from 'react';
+
+const CharacterKey = () => {
+  return <div>CharacterKey COMPONENT</div>;
+  }; 
+
+  export default CharacterKey;
---
+++ src/Components/CharacterKey/__test__/CharacterKey.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import CharacterKey from '../CharacterKey';
+
+describe('CharacterKey component', () => {
+  it('Does something...', () => {
+
+  });
+});
