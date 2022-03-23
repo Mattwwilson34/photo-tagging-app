abc4a5831c05e304759d5af845b02ab5cb02cf34
Matt Wilson <mattwwilson34@gmail.com>
2022-03-22 20:58:16 -0400

    Added conditional logic, images, classes, containers

--- src/Components/CharacterKey/CharacterKey.jsx
+++ src/Components/CharacterKey/CharacterKey.jsx
@@ -1,8 +1,38 @@
-import './CharacterKey.css'
-import React from 'react';
+import './CharacterKey.css';
+import React, { useState } from 'react';
+import Mario from '../../Images/Mario.png';
+import Boo from '../../Images/Boo.png';
+import Luigi from '../../Images/Luigi.png';
+import Toad from '../../Images/Toad.png';
+import Shiek from '../../Images/Shiek.png';
 
 const CharacterKey = () => {
-  return <div>CharacterKey COMPONENT</div>;
-  }; 
+  const [open, setOpen] = useState(true);
 
-  export default CharacterKey;
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
+  if (open) {
+    return (
+      <div className='CharacterKey'>
+        <div className='CharacterKey-ImageWrapper'>
+          <img src={Toad} alt='Toad'></img>
+          <img src={Boo} alt='Boo'></img>
+          <img src={Shiek} alt='Shiek'></img>
+          <img src={Mario} alt='Mario'></img>
+          <img src={Luigi} alt='Luigi'></img>
+        </div>
+        <button onClick={handleClick}>Hide</button>
+      </div>
+    );
+  } else {
+    return (
+      <div className='CharacterKey'>
+        <button onClick={handleClick}>Open Key</button>
+      </div>
+    );
+  }
+};
+
+export default CharacterKey;
